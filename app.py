17dd4d99f8354bf2b2fcdf8cd9291d60fe4651d1
yaashhX <yk5505564@gmail.com>
2025-09-04 20:49:07 +0530

    survey form

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=7000)
+
